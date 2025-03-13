e7e2569bc849e73b8aa4f886df055ca2c0f36e46
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2025-03-13 14:26:23 +0100

    tränat på Javascript, variabler,konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,16 @@
+// var tal1 = 12; 
+// var tal2 =42; 
+// var summa=tal1+tal2; 
+// alert(tal1 + " + " + tal2 + " = " + summa); 
+
+var förnamn="Fuck";
+var efternamn="This";
+var ålder = 18; 
+var namn = förnamn + " " + efternamn; 
+// var meddelande = namn + " är " + ålder + " år gammal ";
+// alert(meddelande);
+
+var hobby = "spela altfiol"
+var kostnad = 1000; 
+var meddelande= namn + " gillar " + hobby + "\noch det kostar " + kostnad + "kr per år." ; 
+alert(meddelande);
